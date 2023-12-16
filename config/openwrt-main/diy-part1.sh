b843841304ad1e8da299b8219da9059c4a2eae41
oylnever <60604728+oylnever@users.noreply.github.com>
2023-12-16 12:04:42 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -12,6 +12,11 @@
 # Uncomment a feed source cp from https://github.com/deartb/AC1700-OpenWrt/blob/main/diy-part1.sh
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
