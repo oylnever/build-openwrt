4f4138806b455243e3419927f0e95a554a961270
oylnever <60604728+oylnever@users.noreply.github.com>
2023-12-16 12:03:53 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -9,6 +9,9 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# Uncomment a feed source cp from https://github.com/deartb/AC1700-OpenWrt/blob/main/diy-part1.sh
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
